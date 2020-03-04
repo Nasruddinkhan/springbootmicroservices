24dbd0930226a5e3b70241452736a9f58a3e80ec
Nasruddinkhan <nasruddinkhan44@gmail.com>
2020-03-05 02:16:58 +0530

    create spring clould config for the envoirment

---
+++ spring-cloud-config-service/src/main/java/com/mypractice/microservices/springcloudconfigservice/SpringCloudConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mypractice.microservices.springcloudconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServiceApplication.class, args);
+	}
+
+}
