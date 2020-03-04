4ad2be363f0cf6a71b65a36c29c0ecc7b3edc955
Nasruddinkhan <nasruddinkhan44@gmail.com>
2020-03-05 03:38:28 +0530

    complete course

---
+++ netflix-zuul-api-getway-server/src/test/java/com/mypractice/microservices/netflixzuulapigetwayserver/NetflixZuulApiGetwayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypractice.microservices.netflixzuulapigetwayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixZuulApiGetwayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- spring-cloud-config-service/src/main/java/com/mypractice/microservices/springcloudconfigservice/SpringCloudConfigServiceApplication.java
+++ spring-cloud-config-service/src/main/java/com/mypractice/microservices/springcloudconfigservice/SpringCloudConfigServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
+
 public class SpringCloudConfigServiceApplication {
 
 	public static void main(String[] args) {
